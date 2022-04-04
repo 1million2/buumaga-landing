328b976957b051328b3bb35b56be24b20f938978
Eduard Makarichak <makarichak1209@gmail.com>
2022-04-04 12:02:22 +0300

    fead: add tingle popup

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+
 const popup = document.querySelector(".popup");
 const body = document.querySelector('body');
 const headerContact = document.querySelector('.header_btn-phone');
 
-
 document.addEventListener('click', (e) => {
     if (e.target.closest(".order-btn") || e.target.closest(".buy")) {
         e.preventDefault()
-        popup.classList.add('active');
-        body.style.overflow = 'hidden';
+        modal.open();
+        // popup.classList.add('active');
+        // body.style.overflow = 'hidden';
     }
     
 })
@@ -15,12 +16,38 @@ document.addEventListener('click', (e) => {
 document.addEventListener('click', (e) => {
     if (e.target.closest(".close")) {
         e.preventDefault()
-        popup.classList.remove('active');
-        body.style.overflow = 'auto';
+        modal.close();
+        // popup.classList.remove('active');
+        // body.style.overflow = 'auto';
     } 
 })
 
 headerContact.addEventListener('click', (e) => {
     document.querySelector('.phone-number').classList.toggle('active')
 })
+
+
+// instanciate new modal
+var modal = new tingle.modal({
+    footer: false,
+    stickyFooter: false,
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: "Закрыть",
+    cssClass: ['custom-class-1', 'custom-class-2'],
+    onOpen: function() {
+        console.log('modal open');
+    },
+    onClose: function() {
+        console.log('modal closed');
+    },
+    beforeClose: function() {
+        // here's goes some logic
+        // e.g. save content before closing the modal
+        return true; // close the modal
+        return false; // nothing happens
+    }
+});
+
+
+modal.setContent('<p class="popup_text">Колличество и наличие уточняйте по телефону</p> \n <a class="popup_tel" href="tel:+375291579017">+375 (29) 157-90-17</a>');
 
